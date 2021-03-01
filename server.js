96022bb20e06552df9bdb44c273bb474f6fe49dd
Brent Abruzese <brent.abruzese@gmail.com>
2021-03-01 15:15:12 -0500

    setup server

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 8080 || process.env.PORT;
 
+app.get('/azuga-data', (req, res) => {
+  console.log(req);
+})
+
+app.listen(port, console.log(`azuga-dev-server is listening on ${port}`));
