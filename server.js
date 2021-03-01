efa1512a1fa38a9d5e513093565000ecfc382801
Brent Abruzese <brent.abruzese@gmail.com>
2021-03-01 15:42:08 -0500

    server

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 8080 || process.env.PORT;
+const AZUGA_URL = 'https://api.azuga.com/azuga-ws/v1/live/location.json'
+const API_KEY = "YWY5M2M2MTQtYjExYi00YWZkLTlhOWItM2E5NzVlY2IzNGVl"
+
+app.use(express.json());
 
 app.get('/azuga-data', (req, res) => {
-  console.log(req);
+  axios({
+    method: 'get',
+    url: AZUGA_URL,
+    headers: {
+      'Authorization': `${API_KEY}`
+    }
+  }).then(response => { 
+    console.log(response.data);
+    res.json(response.data);
+  })
 })
 
-app.listen(port, console.log(`azuga-dev-server is listening on ${port}`));
+app.listen(port, console.log(`azuga-dev-server is listening on port: ${port}`));
