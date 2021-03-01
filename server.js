0112eab9ead28560ebb1ca734fcfef7b1884645d
Brent Abruzese <brent.abruzese@gmail.com>
2021-03-01 16:02:33 -0500

    added cors

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 const AZUGA_URL = 'https://api.azuga.com/azuga-ws/v1/live/location.json'
 const API_KEY = "YWY5M2M2MTQtYjExYi00YWZkLTlhOWItM2E5NzVlY2IzNGVl"
+const cors = require('cors');
 
+app.use(cors);
 app.use(express.json());
 
 app.get('/azuga-data', async (req, res) => {
